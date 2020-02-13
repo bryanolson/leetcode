a0c985d91d6c227f9386f3625903dd4837ec880f
Bryan Olson <bryanolson@bryans-mbp.ad.sannet.gov>
2020-02-13 15:41:40 -0800

    removed uncategorized files

--- 961.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
-        A = sorted(A)
-        leng = int(len(A)/2)
-        if A[leng] == A[leng+1]:
-            return A[leng]
-        else:
-            return A[leng-1]
-        
